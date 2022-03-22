df522b35b56a2cccb850e9629703b83050e10dea
Aleksandr Lakhonin <73431274+Lakhonin@users.noreply.github.com>
2022-03-23 01:35:51 +0300

    Update trigger_1.sql

--- Databases/trigger_1.sql
+++ Databases/trigger_1.sql
@@ -1,2 +1,12 @@
 -- Триггер 1 
 -- при создании заявки  E15, изменяется количество издания в E3
+
+CREATE TRIGGER E15_INSERT_UPDATE
+ON Заказ_в_акте_приема_E15
+WITH EXECUTE AS CALLER
+AFTER INSERT
+AS
+BEGIN
+UPDATE Издание_E3
+SET count = count + (SELECT SUM(count_publication) FROM inserted I where I.id_publication = Издание_E3.id_publication)
+END
