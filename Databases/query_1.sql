ce4282a8f17130b8150682f91fc1854c13a3ed15
Aleksandr Lakhonin <73431274+Lakhonin@users.noreply.github.com>
2022-03-26 02:13:48 +0300

    Update query_1.sql

--- Databases/query_1.sql
+++ Databases/query_1.sql
@@ -8,12 +8,17 @@
 -- 6. количество издания из предыдущего пункта
 -- сортировка по дате первой заявки кафедры (возрастание)
 
-select e7.title 'Название кафедры', e7.name_head + ' ' + e7.surname_head + ' ' + e7.middlename_head 'ФИО заведующего кафедры'
-case when a.qty is not null then a.qty else '0' end 'Количество заявок',
-case when b.qty is not null then b.qty else '0' end 'Общее количество изданий в заявках'
---case when c.qty is not null then c.qty else '0' end 'Название и автор издания чаще заказанного',
---case when d.qty is not null then d.qty else '0' end 'Кол-во издания чаще заказанного'
-from Кафедра_E7 e7 full join Заявка_E8 e8 on e8.id_chair = e7.id_chair
-left outer join (select Count(e8.id_task) qty, e8.id_chair from Заявка_E8 e8 group by e8.id_chair) a on a.id_chair = e7.id_chair
-left outer join (select SUM(e14.count_publication) qty, e14.id_task from Заявки_в_заказе_E14 e14 group by e14.id_task) b on b.id_task = e8.id_task
-
+select 
+--*,
+e7.title 'Название кафедры',
+case when e7.middlename_head is not null then e7.surname_head + ' ' + e7.name_head + ' ' + e7.middlename_head  else e7.surname_head + ' ' + e7.name_head end  'ФИО заведующего кафедры',
+case when a.count_task is not null then a.count_task else '0' end 'Количество заявок',
+case when a.count_publication is not null then a.count_publication else '0' end 'Общее количество изданий в заявках'
+--case when c.qty is not null then c.qty else '0' end 'Название и автор чаще заказанного издания',
+--case when d.qty is not null then d.qty else '0' end 'Кол-во чаще заказанного издания'
+from Кафедра_E7 e7
+left join (select max(e8.date_task) last_task_time,e8.id_chair,count(e8.id_task) count_task,sum(e14.count_publication) count_publication
+			from Заявка_E8 e8 left join Заявки_в_заказе_E14 e14 on e14.id_task = e8.id_task
+			group by e8.id_chair
+			) a on a.id_chair = e7.id_chair
+order by a.last_task_time asc
