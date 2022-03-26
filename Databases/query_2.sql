97e38c02f4d3134bf47ffdf9909bf61b3d7a3226
Aleksandr Lakhonin <73431274+Lakhonin@users.noreply.github.com>
2022-03-26 19:40:40 +0300

    Update query_2.sql

--- Databases/query_2.sql
+++ Databases/query_2.sql
@@ -6,3 +6,11 @@
 -- 3. количество принятых изданий в месяц
 -- 4. количество списанных изданий в месяц
 -- 5. ФИО сотрудника оформившего больше документов в месяц
+
+select e8.id_task, datepart(month, e8.date_task) month_date, year(e8.date_task) year_date, e8.id_employee from Заявка_E8 e8
+
+select e5.id_employee, datepart(month, e5.date_reception) month_date, year(e5.date_reception) year_date, e15.count_publication from Акт_приема_E5 e5
+left join Заказ_в_акте_приема_E15 e15 on e15.id_act_reception = e5.id_act_reception
+
+select * from Акт_списания_E6 e6
+left join Списанная_литература_E12 e12 on e12.id_act_debiting = e6.id_act_debiting
