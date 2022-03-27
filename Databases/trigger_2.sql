8fd6e5bef8765a17951bcfb2cd0eb13a55e2ecb3
Aleksandr Lakhonin <73431274+Lakhonin@users.noreply.github.com>
2022-03-27 19:27:27 +0300

    Update trigger_2.sql

--- Databases/trigger_2.sql
+++ Databases/trigger_2.sql
@@ -1,39 +1,54 @@
--- Триггер 2
--- При создании заявки в отдел продаж проверяет, не превышает ли количество запрашиваемых изданий количества изданий в плане издательств за текущий год
--- выдает ошибки/оповещения 
--- обязательное использование курсора (планы)
--- в E13 добавить атрибут "дата"
-
 create trigger triger2
 on Заказы_у_отдела_продаж_E17
-instead of insert as
+instead of insert
+as
 begin
-	declare @id_publication int, @id_plans_publishing int, @count_publication int, @date date
-	declare @data_now date = '2022'
-	declare e13_crsr CURSOR
-	for select id_publication, id_plans_publishing,count_publication, date from Издаваемая_литература_E13
-	open e13_crsr
-	FETCH NEXT FROM e13_crsr into @id_publication, @id_plans_publishing,@count_publication,@date
-	WHILE(@@FETCH_STATUS=0)
-	begin
-		if(@id_publication != inserted.id_publication)
+	declare @i int
+	declare @id_publication int, @id_plans_publishing int, @count_publication int, @date_plans date
+	declare @tr_id_sales_department int, @tr_id_publication int ,@tr_id_task int,@tr_id_purchase_order int, @tr_count_publication int
+	declare @msg varchar(50)
+	declare tr2_crsr cursor for select id_publication, id_plans_publishing,count_publication, date_plans from Издаваемая_литература_E13
+	set @i = (select max(a.t) from (select row_number() over(order by i.id_publication) t, * from inserted i) a) 
+	while (@i != 0)
 		begin
-			print '1'
+			set @tr_id_publication = (select a.id_publication from (select row_number() over(order by i.id_publication) t, * from inserted i) a where a.t = @i)
+			set @tr_count_publication =(select a.count_publication from (select row_number() over(order by i.id_publication) t, * from inserted i) a where a.t = @i)
+			open tr2_crsr
+			fetch next from tr2_crsr into @id_publication, @id_plans_publishing,@count_publication,@date_plans
+			while(@@FETCH_STATUS=0)
+			begin
+				if(@tr_id_publication = @id_publication)
+				begin
+					if(@date_plans < getdate())
+					begin
+						if(@tr_count_publication < @count_publication)
+						begin
+							set @tr_id_sales_department = (select a.id_sales_department from (select row_number() over(order by i.id_publication) t, * from inserted i) a where a.t = @i)
+							set @tr_id_purchase_order = (select a.id_purchase_order from (select row_number() over(order by i.id_publication) t, * from inserted i) a where a.t = @i)
+							set @tr_id_task = (select a.id_task from (select row_number() over(order by i.id_publication) t, * from inserted i) a where a.t = @i)
+							insert into Заказы_у_отдела_продаж_E17(id_sales_department,id_task,id_purchase_order,id_publication,count_publication)
+							values (@tr_id_sales_department,@tr_id_task,@tr_id_purchase_order,@tr_id_publication,@tr_count_publication)
+							set @msg = 'запись добавлена'
+						end
+						else
+						begin
+							set @msg = 'Количество изданий в планах издательства меньше чем заказаное число изданий. Максимальное: ' + @count_publication
+						end
+					end
+					else
+					begin
+						set @msg = 'Указанное издание издательство в текущем году не издает'	
+					end
+				end
+				else
+				begin
+					set @msg = 'Указанное издание издательство никогда не издавало'
+				end
+			fetch next from tr2_crsr into @id_publication, @id_plans_publishing,@count_publication,@date_plans		
+			end
+		close tr2_crsr
+		set @i = @i -1
+		print @msg
 		end
-		else if(@data_now != @date)
-		begin
-			print '2'
-		end
-		else if(@count_publication < inserted.count_publication)
-		begin
-			print 'Количество издания превышает количества выпущенного издания в текущем году'
-		end	
-		else
-		begin
-			print 'Количество издания не превышает количества выпущенного издания в текущем году'	
-		end
-		FETCH NEXT FROM e13_crsr into @id_publication, @id_plans_publishing,@count_publication,@date
-	end
-	ClOSE e13_crsr
-	DEALLOCATE e13_crsr
+	deallocate tr2_crsr
 end
