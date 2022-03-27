92b4dfd7cc1f4f4646806b8f86776b85dbd30de9
Aleksandr Lakhonin <73431274+Lakhonin@users.noreply.github.com>
2022-03-27 17:26:38 +0300

    Update trigger_1.sql

--- Databases/trigger_1.sql
+++ Databases/trigger_1.sql
@@ -1,12 +1,17 @@
 -- Триггер 1 
 -- при создании заявки  E15, изменяется количество издания в E3
-
-CREATE TRIGGER triger1
-ON Заказ_в_акте_приема_E15
-WITH EXECUTE AS CALLER
-AFTER INSERT
-AS
-BEGIN
-UPDATE Издание_E3
-SET count = count + (SELECT SUM(count_publication) FROM inserted I where I.id_publication = Издание_E3.id_publication)
-END
+create trigger triger1
+on Заказ_в_акте_приема_E15
+after insert
+as
+begin
+	declare @i int
+	set @i = (select max(a.t) from (select row_number() over(order by i.id_act_reception) t, * from inserted i) a) 
+	while (@i != 0)
+		begin
+			update Издание_E3
+			set count_publication = count_publication + (select a.count_publication from (select row_number() over(order by i.id_act_reception) t, * from inserted i) a where a.t = @i)
+			where id_publication = (select a.id_publication from (select row_number() over(order by i.id_act_reception) t, * from inserted i) a where a.t = @i)
+			set @i = @i -1
+		end
+end
