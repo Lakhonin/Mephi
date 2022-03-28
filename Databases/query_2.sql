627ab6656e790574c178c2a804732470fd45f336
Aleksandr Lakhonin <73431274+Lakhonin@users.noreply.github.com>
2022-03-28 03:28:47 +0300

    Update query_2.sql

--- Databases/query_2.sql
+++ Databases/query_2.sql
@@ -7,31 +7,25 @@
 -- 4. количество списанных изданий в месяц
 -- 5. ФИО сотрудника оформившего больше документов в месяц
 
-select e8.id_task, datepart(month, e8.date_task) month_date, year(e8.date_task) year_date, e8.id_employee from Заявка_E8 e8
-
-select e5.id_employee, datepart(month, e5.date_reception) month_date, year(e5.date_reception) year_date, e15.count_publication from Акт_приема_E5 e5
-left join Заказ_в_акте_приема_E15 e15 on e15.id_act_reception = e5.id_act_reception
-
-select e6.id_employee, datepart(month, e6.date_debiting) month_date,year(e6.date_debiting) year_date,e12.count_publication from Акт_списания_E6 e6
-left join Списанная_литература_E12 e12 on e12.id_act_debiting = e6.id_act_debiting
 
 
-select *,
-coalesce(count(a.id_employee) over (partition by a.id_employee,a.month_date,a.year_date order by a.month_date
-			rows between unbounded preceding and current row), 0) sum_id_employee
-from (
-select e4.id_employee, datepart(month, e4.date_registration) month_date,year(e4.date_registration) year_date from Заказ_на_приобретение_литературы_E4 e4
-union
-select e8.id_employee, datepart(month, e8.date_task) month_date, year(e8.date_task) year_date from Заявка_E8 e8
-union
-(select e5.id_employee, datepart(month, e5.date_reception) month_date, year(e5.date_reception) year_date from Акт_приема_E5 e5
-left join Заказ_в_акте_приема_E15 e15 on e15.id_act_reception = e5.id_act_reception)
-union
-(select e6.id_employee, datepart(month, e6.date_debiting) month_date,year(e6.date_debiting) year_date from Акт_списания_E6 e6
-left join Списанная_литература_E12 e12 on e12.id_act_debiting = e6.id_act_debiting)
-)a
-order by a.month_date
+select * from (
+				select e8.id_task task, datepart(month, e8.date_task) month_date, year(e8.date_task) year_date, e8.id_employee employee,0 count_pub, 1 ch from Заявка_E8 e8
+				
+	
+	union 
+				select 0 task, datepart(month, e5.date_reception) month_date, year(e5.date_reception) year_date, e5.id_employee employee, e15.count_publication count_pub, 2 ch from Акт_приема_E5 e5
+				left join Заказ_в_акте_приема_E15 e15 on e15.id_act_reception = e5.id_act_reception
+				
+	union 
+				select 0 task,datepart(month,e4.date_registration) month_date, datepart(year,e4.date_registration) year_date,e4.id_employee employee,0 count_pub,4 ch from Заказ_на_приобретение_литературы_E4 e4
+		)a
+	where a.year_date = (select year(getdate())-1)
+		
+				
+	union 
+							
+	
 
-select * from Заказ_на_приобретение_литературы_E4
-insert into Заказ_на_приобретение_литературы_E4(id_purchase_order,id_employee,date_registration,date_transfer)
-values (21,2,'01.03.2021',null)
+	select 0 task, datepart(month, e6.date_debiting) month_date,year(e6.date_debiting) year_date, e6.id_employee employee, 12.count_publication count_pub, 3 ch from Акт_списания_E6 e6
+				left join Списанная_литература_E12 e12 on e12.id_act_debiting = e6.id_act_debiting
