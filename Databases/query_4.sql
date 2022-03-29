547c266b0478eb47fe25bc1704998e845593692a
Aleksandr Lakhonin <73431274+Lakhonin@users.noreply.github.com>
2022-03-29 22:58:00 +0300

    Update query_4.sql

--- Databases/query_4.sql
+++ Databases/query_4.sql
@@ -7,6 +7,7 @@
 -- 5. тип изменения ("+" или "-")
 -- 6. дата изменения
 
+
 select 
 a.title_publishing 'Название издательства',
 a.title_publication 'Названия издания',
@@ -22,10 +23,10 @@ from (
 		full join Издаваемая_литература_E13 e13 on e13.id_plans_publishing = e11.id_plans_publishing
 		full join Издание_E3 e3 on e3.id_publication = e13.id_publication
 		) a
-	full join (select e12.id_publication, e6.date_debiting date_operation,e12.count_publication, '-' as type_operation 
+	full join (select e12.id_publication, e6.date_debiting date_operation,-e12.count_publication count_publication, '-' as type_operation 
 			from Списанная_литература_E12 e12 
 			left join Акт_списания_E6 e6 on e6.id_act_debiting = e12.id_act_debiting
-		union
+		union all
 			select e15.id_publication,e5.date_reception date_operation,e15.count_publication, '+' as type_operation
 			from Заказ_в_акте_приема_E15 e15
 			left join Акт_приема_E5 e5 on e15.id_act_reception = e5.id_act_reception) b on b.id_publication = a.id_publication
