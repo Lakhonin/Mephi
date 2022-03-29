9684916aa47d799dd0fcb296e54e1945079412e4
Aleksandr Lakhonin <73431274+Lakhonin@users.noreply.github.com>
2022-03-29 17:52:30 +0300

    Update trigger_2.sql

--- Databases/trigger_2.sql
+++ Databases/trigger_2.sql
@@ -2,17 +2,18 @@
 -- При создании заказа в отдел продаж проверяет, не превышает ли количество запрашиваемых изданий количества изданий в плане издательств за текущий год
 -- выдает ошибки/оповещения 
 -- обязательное использование курсора (планы)
--- в E13 добавить атрибут "дата"
 
+--drop trigger triger2
 create trigger triger2
 on Заказы_у_отдела_продаж_E17
 instead of insert
 as
 begin
 	declare @i int
+	declare @year_now int = (select year(getdate()))
+	declare @t int
 	declare @id_publication int, @id_plans_publishing int, @count_publication int, @date_plans date
 	declare @tr_id_sales_department int, @tr_id_publication int ,@tr_id_task int,@tr_id_purchase_order int, @tr_count_publication int
-	declare @msg varchar(50)
 	declare tr2_crsr cursor for select id_publication, id_plans_publishing,count_publication, date_plans from Издаваемая_литература_E13
 	set @i = (select max(a.t) from (select row_number() over(order by i.id_publication) t, * from inserted i) a) 
 	while (@i != 0)
@@ -25,7 +26,8 @@ begin
 			begin
 				if(@tr_id_publication = @id_publication)
 				begin
-					if(@date_plans < getdate())
+					set @t = year(@date_plans)
+					if(@t = @year_now)
 					begin
 						if(@tr_count_publication < @count_publication)
 						begin
@@ -34,27 +36,39 @@ begin
 							set @tr_id_task = (select a.id_task from (select row_number() over(order by i.id_publication) t, * from inserted i) a where a.t = @i)
 							insert into Заказы_у_отдела_продаж_E17(id_sales_department,id_task,id_purchase_order,id_publication,count_publication)
 							values (@tr_id_sales_department,@tr_id_task,@tr_id_purchase_order,@tr_id_publication,@tr_count_publication)
-							set @msg = 'запись добавлена'
+							print 'E17: <Заказы_у_отдела_продаж> вставлена.'
 						end
 						else
 						begin
-							set @msg = 'Количество изданий в планах издательства меньше чем заказаное число изданий. Максимальное: ' + @count_publication
+							print 'Ошибка: Количество изданий в планах издательства меньше чем заказаное число изданий.'
 						end
 					end
 					else
 					begin
-						set @msg = 'Указанное издание издательство в текущем году не издает'	
+						print 'Ошибка: Указанное издание издательство в текущем году не издает.'	
 					end
-				end
-				else
-				begin
-					set @msg = 'Указанное издание издательство никогда не издавало'
 				end
 			fetch next from tr2_crsr into @id_publication, @id_plans_publishing,@count_publication,@date_plans		
 			end
 		close tr2_crsr
 		set @i = @i -1
-		print @msg
 		end
 	deallocate tr2_crsr
 end
+
+insert into Заказы_у_отдела_продаж_E17(id_sales_department,id_task,id_purchase_order,id_publication,count_publication)
+values('2','16','4','8','50'),
+('3','16','4','4','452'),
+('4','16','4','5','200'),
+
+
+select * from Заявки_в_заказе_E14
+select * from Издаваемая_литература_E13
+select * from Заказы_у_отдела_продаж_E17
+select * from dbo.Заявка_E8
+select * from Отдел_продаж_E9
+
+insert into Заявки_в_заказе_E14(id_purchase_order,id_task,id_publication,count_publication)
+values ('4','16','8','50'),
+('4','16','4','452') ,
+('4','16','5','200')
