977ac02bdaacf059de055ca2d7004bd752b2ab6a
Aleksandr Lakhonin <73431274+Lakhonin@users.noreply.github.com>
2022-06-19 19:07:09 +0300

    Update helloworld.py

--- Fundamentals of Robotics/helloworld.py
+++ Fundamentals of Robotics/helloworld.py
@@ -1,8 +1,3 @@
 def your_function(greeting):
   result = (greeting + ' ' + "World!")
   return result
-
-for greeting in ["Hello", "Hi", "Привет"]:
-  print("Greeting: ", greeting)
-  result = your_function(greeting)
-  print("Result: ", result)
